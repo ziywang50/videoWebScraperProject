80b8fbb259e1dcfafe374b554544d869b616f612
unknown <ziywang50@gmail.com>
2024-11-12 17:45:18 -0800

    second

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 
 def example(video_link, json_path, CLEAR_JSON=True):
-    #JSON_PATH = "product_info.json"
     PATH = "C:/Program Files/chromedriver-win64/chromedriver.exe"
     REDIRECT = "https://www.youtube.com/redirect?"
     AMAZON = "https://www.amazon.com"
@@ -45,12 +44,21 @@ def example(video_link, json_path, CLEAR_JSON=True):
                     driver.find_element(By.XPATH, '//*[@id="invalid-token-redirect-goto-site-button"]').click()
                     #break
                 current_url = driver.current_url
+                for s in SOCIAL_MEDIA_URLS:
+                    if (current_url.find(s) == 0):
+                        time.sleep(2)
+                        driver.quit()
                 if (current_url.find(AMAZON) == 0):
                     amzn_web(driver, json_objs)
-                time.sleep(3)
+                time.sleep(2)
                 driver.quit()
-    with open(json_path, "w") as outfile:
-        json.dump(json_objs, outfile)
+    #By default, it clears the current json with the same name.
+    if (CLEAR_JSON):
+        with open(json_path, "w") as outfile:
+            json.dump(json_objs, outfile)
+    else:
+        with open(json_path, "a") as outfile:
+            json.dump(json_objs, outfile)
         #if (driver.find_elements(By.XPATH, "//*[@id='items']/ytd-merch-shelf-item-renderer")):
     #WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, "style-scope ytd-merch-shelf-item-renderer")))
     driver = webdriver.Chrome()
@@ -75,28 +83,30 @@ def amzn_web(driver, json_objs):
     PRICE_FRACTION_CLASS = 'a-price-fraction'
     try:
         price = driver.find_element(By.ID, PRICE_ID)
-        if (price):
-            whole_class = price.find_element(By.CLASS_NAME, PRICE_WHOLE_CLASS)
-            symbol_class = price.find_element(By.CLASS_NAME, PRICE_SYMBOL_CLASS)
-            fraction_class = price.find_element(By.CLASS_NAME, PRICE_FRACTION_CLASS)
-            pricef = symbol_class.text + whole_class.text + '.' + fraction_class.text
-        else:
-            pricef = driver.find_element(By.XPATH, KINDLE_PRICE_PATH)
-        img = driver.find_element(By.XPATH, IMG_PATH)
-        title = driver.find_element(By.XPATH, TITLE_PATH)
-        info = {
-            "product_price":  pricef,
-            "product_image": img.get_attribute('src'),
-            "product_name": title.text,
-            "buy_link": current_url
-        }
-        #with open(json_path, "a") as outfile:
-        json_objs.append(info)
-        #print("The price of the product is:", symbol_class.text, whole_class.text, '.', fraction_class.text, "with image link", img.get_attribute('src'))
-        print("Successful")
+        whole_class = price.find_element(By.CLASS_NAME, PRICE_WHOLE_CLASS)
+        symbol_class = price.find_element(By.CLASS_NAME, PRICE_SYMBOL_CLASS)
+        fraction_class = price.find_element(By.CLASS_NAME, PRICE_FRACTION_CLASS)
+        pricef = symbol_class.text + whole_class.text + '.' + fraction_class.text
+
     except:
-        print("Failed")
-    return
+        try:
+            price = driver.find_element(By.XPATH, KINDLE_PRICE_PATH)
+            pricef = price.text
+        except:
+            print("Find price failed")
+            return
+    img = driver.find_element(By.XPATH, IMG_PATH)
+    title = driver.find_element(By.XPATH, TITLE_PATH)
+    info = {
+        "product_price":  pricef,
+        "product_image": img.get_attribute('src'),
+        "product_name": title.text,
+        "buy_link": current_url
+    }
+    #with open(json_path, "a") as outfile:
+    json_objs.append(info)
+    #print("The price of the product is:", symbol_class.text, whole_class.text, '.', fraction_class.text, "with image link", img.get_attribute('src'))
+    #print("Successful")
 
 if __name__ == '__main__':
     LINK_ZERO = 'https://www.youtube.com/watch?v=qm1BBmV8RRY'
@@ -107,8 +117,8 @@ if __name__ == '__main__':
     #JSON_PATH_2 = 'product_info_2.json'
     #JSON_PATH_3 = 'product_info_3.json'
     JSON_PATH_4 = 'product_info_4.json'
-    example(LINK_ZERO, JSON_PATH_1)
-    #example(AMZN_LINK_TWO, JSON_PATH_4)
+    #example(LINK_ZERO, JSON_PATH_1)
+    example(AMZN_LINK_TWO, JSON_PATH_4)
     #example(LINK_ONE, JSON_PATH_2)
     #example(AMZN_LINK_ZERO, JSON_PATH_3)
     #example('https://www.youtube.com/watch?v=ju8OkY4EE24')
